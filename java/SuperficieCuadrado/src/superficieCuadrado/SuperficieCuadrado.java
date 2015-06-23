4cade6ed3c9361af4d2db85f3e7042a30800cec0
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-06-23 17:35:52 -0300

    Se agrega codigo/practica de SuperficieCuadrado

---
+++ java/SuperficieCuadrado/src/superficieCuadrado/SuperficieCuadrado.java
@@ -0,0 +1,19 @@
+package superficieCuadrado;
+
+import java.util.Scanner;
+
+public class SuperficieCuadrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		int lado;
+		int superficie;
+		System.out.print("Ingrese el valor del lado del cuadrado:");
+		lado=teclado.nextInt();
+		superficie=lado*lado;
+		System.out.print("La superficie del cuadrado es:");
+		System.out.print(superficie);
+	}
+
+}
