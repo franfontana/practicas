13064afd0d66115596ffb3c44043f80ea8c4013f
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-07-10 16:09:41 -0300

    Se agredo codigo de Promedio3NotasAlumno

--- java/Promedio3NotasAlumno/src/promedio3NotasAlumno/Promedio3NotasAlumno.java
+++ java/Promedio3NotasAlumno/src/promedio3NotasAlumno/Promedio3NotasAlumno.java
@@ -6,7 +6,19 @@ public class Promedio3NotasAlumno {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
-		
+		float calificacion1, calificacion2, calificacion3;
+    	System.out.print("Ingrese la primer calificacion del alumno:");
+    	calificacion1=teclado.nextInt();
+    	System.out.print("Ingrese la segunda calificacion del alumno:");
+    	calificacion2=teclado.nextInt();
+    	System.out.print("Ingrese la tercer calificacion del alumno:");
+    	calificacion3=teclado.nextInt();
+        float promedio;
+        promedio=(calificacion1 + calificacion2 + calificacion3) / 3;
+        System.out.println("El promedio del alumno es:" + promedio);
+    	if (promedio >= 7) {
+    	    System.out.print("Promocionado");
+    	}
 	}
 
 }
