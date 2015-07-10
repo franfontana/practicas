07380af787ff73dd8d343971d705de5f563246cd
Francisco Fontana <francisco.fontana@mercadolibre.com>
2015-07-10 12:59:56 -0300

    Agrego codigo de Promedio3NotasAlumno

---
+++ java/Promedio3NotasAlumno/src/promedio3NotasAlumno/Promedio3NotasAlumno.java
@@ -0,0 +1,12 @@
+package promedio3NotasAlumno;
+
+import java.util.Scanner;
+public class Promedio3NotasAlumno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado=new Scanner(System.in);
+		
+	}
+
+}
